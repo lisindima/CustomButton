47e7dea3f98dc68533f20953e1a971333c6f0741
lisindima <lisindima1996@gmail.com>
2019-11-21 10:01:31 +0700

    1.0.2

--- Sources/CustomButton/CustomButton.swift
+++ Sources/CustomButton/CustomButton.swift
@@ -10,11 +10,19 @@ import SwiftUI
 
 @available(iOS 13.0, tvOS 13.0, macOS 10.15, *)
 public struct CustomButton : View {
+    
     var label: String
     var action: () -> Void
     var loading: Bool = false
     
-    var body: some View {
+    public init(label: String, action: @escaping () -> Void, loading: Bool = false) {
+        self.label = label
+        self.action = action
+        self.loading = loading
+        
+    }
+    
+    public var body: some View {
         Button(action: action) {
             HStack {
                 Spacer()
