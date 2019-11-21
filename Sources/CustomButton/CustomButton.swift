72584ec30c034cd1334cf44b389ff9b95bfad0dc
lisindima <lisindima1996@gmail.com>
2019-11-21 09:33:14 +0700

    1.0.1

--- Sources/CustomButton/CustomButton.swift
+++ Sources/CustomButton/CustomButton.swift
@@ -1,6 +1,15 @@
+//
+//  CustomButton.swift
+//  CustomButton
+//
+//  Created by Дмитрий Лисин on 20.11.2019.
+//  Copyright © 2019 Дмитрий Лисин. All rights reserved.
+//
+
 import SwiftUI
 
-struct CustomButton : View {
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, *)
+public struct CustomButton : View {
     var label: String
     var action: () -> Void
     var loading: Bool = false
